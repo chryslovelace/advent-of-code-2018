d7f1b68463b5392d3a84b63a1aaf10da1bad2c43
Chrys Lovelace <chryslovelace@gmail.com>
2018-12-01 19:37:43 -0500

    clipy

--- src/d1/mod.rs
+++ src/d1/mod.rs
@@ -1,9 +1,9 @@
-const INPUT: &'static str = include_str!("input.txt");
+const INPUT: &str = include_str!("input.txt");
 
 pub fn parsed_input() -> impl Iterator<Item = i32> + Clone {
     INPUT.lines().map(|line| {
         let mut num: i32 = line[1..].parse().unwrap();
-        if line.starts_with("-") {
+        if line.starts_with('-') {
             num *= -1;
         }
         num
