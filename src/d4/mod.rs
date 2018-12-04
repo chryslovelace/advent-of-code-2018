6f12fb00c486f9d8728de9546236d071dea94544
Chris Lovelace <chryslovelace@gmail.com>
2018-12-04 13:39:37 -0500

    more refactoring

--- src/d4/mod.rs
+++ src/d4/mod.rs
@@ -1,53 +1,26 @@
 use std::collections::HashMap;
 use scan_fmt::scan_fmt;
 use itertools::Itertools;
-use chrono::{NaiveDateTime, Timelike};
+use chrono::NaiveDateTime;
 
 const INPUT: &str = include_str!("input.txt");
 
-#[derive(Debug)]
-struct Entry {
-    date: NaiveDateTime,
-    kind: EntryKind
-}
-
-#[derive(Debug)]
-enum EntryKind {
-    ShiftStart(u32),
-    FallsAsleep,
-    WakesUp
-}
-
 pub fn sleepytime() -> HashMap<u32, HashMap<u32, u32>> {
-    let entries = INPUT.lines().map(|line| {
-        let date = NaiveDateTime::parse_from_str(&line[1..17], "%Y-%m-%d %H:%M").unwrap();
-        let kind = if let Some(id) = scan_fmt!(&line[19..], "Guard #{d} begins shift", u32) {
-            EntryKind::ShiftStart(id)
-        } else if &line[19..] == "falls asleep" {
-            EntryKind::FallsAsleep
-        } else if &line[19..] == "wakes up" {
-            EntryKind::WakesUp
-        } else {
-            unreachable!()
-        };
-        Entry {date, kind}
-    }).sorted_by_key(|entry| entry.date);
-
-    let mut runs = Vec::new();
-    for entry in entries {
-        if let EntryKind::ShiftStart(_) = entry.kind {
-            runs.push(Vec::new());
-        }
-        runs.last_mut().unwrap().push(entry);
-    }
-
+    let mut entries = INPUT
+        .lines()
+        .sorted_by_key(|line| NaiveDateTime::parse_from_str(&line[1..17], "%Y-%m-%d %H:%M").unwrap())
+        .into_iter()
+        .peekable();
+    
     let mut sleepytime = HashMap::new();
-    for run in runs {
-        let id = if let EntryKind::ShiftStart(id) = run[0].kind { id } else { unreachable!() };
+    while let Some(id_line) = entries.next() {
+        let id = scan_fmt!(&id_line[19..], "Guard #{d} begins shift", u32).unwrap();
         let minutes = sleepytime.entry(id).or_insert_with(HashMap::new);
-        let naps = run[1..].chunks(2).map(|chunk| chunk[0].date.minute()..chunk[1].date.minute());
-        for nap in naps {
-            for minute in nap {
+        while let Some(line) = entries.peek() {
+            if line.ends_with("begins shift") { break; }
+            let sleeps = entries.next().unwrap()[15..17].parse().unwrap();
+            let wakes = entries.next().unwrap()[15..17].parse().unwrap();
+            for minute in sleeps..wakes {
                 *minutes.entry(minute).or_insert(0) += 1;
             }
         }
