f3d5890da492caf43db71e2d532284cbca29a3f3
Chris Lovelace <chryslovelace@gmail.com>
2018-12-04 16:32:22 -0500

    more refactoring

--- src/bin/d4p1.rs
+++ src/bin/d4p1.rs
@@ -2,7 +2,7 @@ use advent_of_code_2018::d4::sleepytime;
 
 fn main() {
     let sleepytime = sleepytime();
-    let (id, minutes) = sleepytime.iter().max_by_key(|(_, minutes)| minutes.values().sum::<u32>()).unwrap();
-    let (minute, _) = minutes.iter().max_by_key(|(_, &count)| count).unwrap();
+    let (id, minutes) = sleepytime.iter().max_by_key(|(_, minutes)| minutes.iter().sum::<u32>()).unwrap();
+    let minute = (0..60).max_by_key(|&i| minutes[i as usize]).unwrap();
     println!("{}", id * minute);
 }
--- src/d4/mod.rs
+++ src/d4/mod.rs
@@ -5,7 +5,7 @@ use chrono::NaiveDateTime;
 
 const INPUT: &str = include_str!("input.txt");
 
-pub fn sleepytime() -> HashMap<u32, HashMap<u32, u32>> {
+pub fn sleepytime() -> HashMap<u32, [u32; 60]> {
     let mut entries = INPUT
         .lines()
         .sorted_by_key(|line| NaiveDateTime::parse_from_str(&line[1..17], "%Y-%m-%d %H:%M").unwrap())
@@ -15,13 +15,13 @@ pub fn sleepytime() -> HashMap<u32, HashMap<u32, u32>> {
     let mut sleepytime = HashMap::new();
     while let Some(id_line) = entries.next() {
         let id = scan_fmt!(&id_line[19..], "Guard #{d} begins shift", u32).unwrap();
-        let minutes = sleepytime.entry(id).or_insert_with(HashMap::new);
+        let minutes = sleepytime.entry(id).or_insert_with(|| [0; 60]);
         while let Some(line) = entries.peek() {
             if line.ends_with("begins shift") { break; }
-            let sleeps = entries.next().unwrap()[15..17].parse().unwrap();
-            let wakes = entries.next().unwrap()[15..17].parse().unwrap();
+            let sleeps: usize = entries.next().unwrap()[15..17].parse().unwrap();
+            let wakes: usize = entries.next().unwrap()[15..17].parse().unwrap();
             for minute in sleeps..wakes {
-                *minutes.entry(minute).or_insert(0) += 1;
+                minutes[minute] += 1;
             }
         }
     }
