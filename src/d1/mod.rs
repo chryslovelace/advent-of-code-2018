abf9bdd85a27ce64940bdf5e7d547c211184ef74
Chrys Lovelace <chryslovelace@gmail.com>
2018-12-06 21:16:00 -0500

    well, this just works i guess

--- src/d1/mod.rs
+++ src/d1/mod.rs
@@ -1,11 +1,5 @@
 const INPUT: &str = include_str!("input.txt");
 
 pub fn frequency_changes() -> impl Iterator<Item = i32> + Clone {
-    INPUT.lines().map(|line| {
-        let mut num: i32 = line[1..].parse().unwrap();
-        if line.starts_with('-') {
-            num *= -1;
-        }
-        num
-    })
+    INPUT.lines().map(|line| line.parse().unwrap())
 }
