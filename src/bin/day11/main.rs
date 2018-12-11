ec56e663911660c33743720da5754e8f870d80c3
Chris Lovelace <chryslovelace@gmail.com>
2018-12-11 09:31:55 -0500

    finished day 11

--- src/bin/day11/main.rs
+++ src/bin/day11/main.rs
@@ -1,38 +1,42 @@
 use itertools::iproduct;
-use std::collections::HashMap;
 use std::cmp::max;
 
-const SERIAL_NUMBER: i64 = 3613;
+const SERIAL_NUMBER: i32 = 3613;
 
-fn get_power((x, y): (i64, i64)) -> i64 {
+fn get_power((x, y): (i32, i32)) -> i32 {
     let rack_id = x + 10;
     (rack_id * y + SERIAL_NUMBER) * rack_id / 100 % 10 - 5
 }
 
 fn part1() {
-    let mut map: HashMap<(i64, i64), i64> = HashMap::new();
-    for (x, y) in iproduct!(1..299, 1..299) {
-        let power = iproduct!(x..x+3, y..y+3).map(get_power).sum();
-        map.insert((x, y), power);
-    }
-    let (point, _) = map.iter().max_by_key(|(_, &power)| power).unwrap();
-    println!("{:?}", point);
+    let (x, y) = iproduct!(1..299, 1..299)
+        .max_by_key(|&(x, y)| iproduct!(x..x + 3, y..y + 3).map(get_power).sum::<i32>())
+        .unwrap();
+    println!("{},{}", x, y);
 }
 
 fn part2() {
-    let mut map: HashMap<(i64, i64, i64), i64> = HashMap::new();
-    for (x, y) in iproduct!(1..=300, 1..=300) {
-        let mut power = 0;
-        for size in 1..=(301 - max(x, y)) {            
-            power += (x..=x+size).zip(y..=y+size).map(get_power).sum::<i64>();
-            map.insert((x, y, size), power);
-        }
-    }
-    let (square, _) = map.iter().max_by_key(|(_, &power)| power).unwrap();
-    println!("{:?}", square);
+    let (x, y, size, _) = iproduct!(1..301, 1..301)
+        .map(|(x, y)| {
+            (1..=(301 - max(x, y)))
+                .scan(0, |power, size| {
+                    *power += (x..x + size)
+                        .map(|xx| get_power((xx, y + size - 1)))
+                        .sum::<i32>()
+                        + (y..y + size - 1)
+                            .map(|yy| get_power((x + size - 1, yy)))
+                            .sum::<i32>();
+                    Some((x, y, size, *power))
+                })
+                .max_by_key(|&(_, _, _, power)| power)
+                .unwrap()
+        })
+        .max_by_key(|&(_, _, _, power)| power)
+        .unwrap();
+    println!("{},{},{}", x, y, size);
 }
 
 fn main() {
     part1();
-    //part2();
+    part2();
 }
