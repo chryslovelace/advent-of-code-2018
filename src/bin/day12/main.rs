2ee1fab6435cca005db22bfe28af2f3b07681320
Chris Lovelace <chryslovelace@gmail.com>
2018-12-12 14:31:41 -0500

    fully code solution

--- src/bin/day12/main.rs
+++ src/bin/day12/main.rs
@@ -1,23 +1,23 @@
 use lazy_static::lazy_static;
-use std::collections::BTreeSet;
+use std::collections::{hash_map::Entry, BTreeSet, HashMap};
 
 #[derive(Debug, Clone)]
-struct State(BTreeSet<isize>);
+struct State(BTreeSet<i64>);
 
 impl State {
-    fn has_plant(&self, pos: isize) -> bool {
+    fn has_plant(&self, pos: i64) -> bool {
         self.0.contains(&pos)
     }
 
-    fn min(&self) -> isize {
+    fn min(&self) -> i64 {
         *self.0.iter().next().unwrap()
     }
 
-    fn max(&self) -> isize {
+    fn max(&self) -> i64 {
         *self.0.iter().next_back().unwrap()
     }
 
-    fn matches(&self, pos: isize, rule: &Rule) -> bool {
+    fn matches(&self, pos: i64, rule: &Rule) -> bool {
         rule.pattern
             .iter()
             .zip(pos - 2..)
@@ -44,8 +44,18 @@ impl State {
         }
     }
 
-    fn sum(&self) -> isize {
+    fn sum(&self) -> i64 {
         self.0.iter().sum()
+    }
+
+    fn signature(&self) -> String {
+        (self.min()..=self.max())
+            .map(|i| if self.has_plant(i) { '#' } else { '.' })
+            .collect()
+    }
+
+    fn shift(&self, amount: i64) -> State {
+        State(self.0.iter().map(|i| i + amount).collect())
     }
 }
 
@@ -62,7 +72,7 @@ lazy_static! {
             INPUT.lines().next().unwrap()[15..]
                 .chars()
                 .enumerate()
-                .filter_map(|(i, c)| if c == '#' { Some(i as isize) } else { None })
+                .filter_map(|(i, c)| if c == '#' { Some(i as i64) } else { None })
                 .collect(),
         )
     };
@@ -90,9 +100,32 @@ fn part1() {
     println!("{}", state.sum());
 }
 
+const FIFTY_BILLION: i64 = 50_000_000_000;
+
 fn part2() {
-    // i found this out by printing the sum every 1k steps and finding the pattern
-    println!("{}", 50_000_000_000isize * 59 + 1598);
+    // thanks to jewel for this idea
+    // in this case the cycle length happens to be 1 so most of this is unnecessary,
+    // but presumably this would work for any cycle length
+    let mut state = INITIAL_STATE.clone();
+    let mut seen = HashMap::new();
+    let mut step = 0;
+    let (cycle_start, start_min) = loop {
+        match seen.entry(state.signature()) {
+            Entry::Vacant(entry) => entry.insert((step, state.min())),
+            Entry::Occupied(entry) => break entry.remove(),
+        };
+        step += 1;
+        state.next_state(&*RULES);
+    };
+    let cycle_length = step - cycle_start;
+    let cycle_shift = state.min() - start_min;
+    let num_cycles = (FIFTY_BILLION - cycle_start) / cycle_length - 1;
+    state = state.shift(num_cycles * cycle_shift);
+    step += num_cycles * cycle_length;
+    for _ in step..FIFTY_BILLION {
+        state.next_state(&*RULES);
+    }
+    println!("{}", state.sum());
 }
 
 fn main() {
